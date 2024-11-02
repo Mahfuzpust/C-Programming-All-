00ad94f5cacf4bd07bc949f1c39c6f0cbeb7414e
Md. Mahfuzur Rahman <44641277+Mahfuzpust@users.noreply.github.com>
2024-11-02 20:23:55 +0600

    Update 1.3.c

--- First Assignment/1.3.c
+++ First Assignment/1.3.c
@@ -1,19 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int i,num;
+    int i,num, flag = 0;
     scanf("%d",&num);
     for(i=2;i<num;i++)
     {
         if(num%i==0)
         {
-            printf("number is not prime");
-       break;
+            flag = 1;
+           break;
         }
     }
-    if(i==i);
+
+    if(flag == 0)
        {
-       printf("number is prime");
+          printf("number is prime");
+       }
+    else{
+        printf("number is not prime");
        }
 
        return 0;
